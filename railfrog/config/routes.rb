a72f7912bca9a4b0384a5174ed7965788fcbde6a
Fedorov Dmitriy <dlfedorov@gmail.com>
2009-07-01 21:53:26 +0400

    working admin section

--- railfrog/config/routes.rb
+++ railfrog/config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
 # Default route for DB lookup
-  map.map_plugin '/admin', :controller => 'railfrog_admin'
+  map.rf_admin '/admin', :controller => 'railfrog_admin'
   map.map_plugin '*path', :controller => 'site_mapper', :action => 'show_chunk'
 end
